0315927f498b9d4f7a47a8014438756296163a26
Camile Stefany <131724367+camsste@users.noreply.github.com>
2024-06-30 13:51:55 -0300

    Add files via upload

---
+++ UsandoFor.py
@@ -0,0 +1,4 @@
+for floor in range(20, 0, -1):
+    if floor == 13:
+        continue
+    print(floor)
---
+++ UsandoWhile.py
@@ -0,0 +1,7 @@
+floor = 20
+while floor > 0:
+    if floor == 13:
+        floor -= 1
+        continue
+    print(floor)
+    floor -= 1
